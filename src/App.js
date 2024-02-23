454fd1961bfd6c8f9da1c75e33fa347ff1b9532f
savan <savannahwilliams243@gmail.com>
2024-02-22 17:06:04 -0800

    updated About section

--- src/App.js
+++ src/App.js
@@ -9,23 +9,38 @@ import { Helmet } from "react-helmet";
 import "./App.css";
 
 function App() {
+  const [currentTab, handleTabChange] = useState("about");
+
+  // This method is checking to see what the value of `currentTab` is. Depending on the value of currentTab, we return the corresponding component to render.
+  const renderTab = () => {
+    if (currentTab === "About") {
+      return <About />;
+    }
+    if (currentTab === "Contact") {
+      return <Contact />;
+    }
+    if (currentTab === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentTab === "Resume") {
+      return <Resume />;
+    }
+    return <About />;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Helmet>
+        <title>Savannah Williams' Portfolio | {currentTab} </title>
+      </Helmet>
+      ;
+      <Header
+        currentTab={currentTab}
+        handleTabChange={handleTabChange}
+      ></Header>
+      <main>{renderTab()}</main>
+      <Footer></Footer>
+    </>
   );
 }
 
---
+++ src/components/About/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+// import profileImage from "../../assets/large/";
+function About() {
+  return (
+    <section id="about-me" className="my-5 intro">
+      <div class="flex-row">
+        <h1 id="about" className="section-title primary-border">
+          About Me
+        </h1>
+      </div>
+      <div className="flex-row">
+        <div class="intro-info">
+          <div class="intro-img">
+        {/* <img src={profileImage} style={{ width: "100%" }} alt="profile" /> */}
+          </div>
+          <p>
+            Hi there! I'm Savannah Williams, a 26-year-old from the Bay Area. I have a BA in Theology and Religious Studies from Saint Mary's College of California. Although most of my academic background revolves around Theology, I have decided to pursue coding!
+          </p>
+          <p>Beyond my professional commitments, I enjoy anything and everything that involves art. Music, poetry, fashion, and culinary arts all inspire me to live life to the fullest.</p>
+          <p>The things that make me happiest aside from coding are cups of chai, the winter season, and my dog Finnick.</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;

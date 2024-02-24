7833270d52d442c71047620b0aec14b0a3f866fc
savan <savannahwilliams243@gmail.com>
2024-02-24 11:18:35 -0800

    created a form submit handler for preventing default behavior

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -25,4 +25,27 @@ function Contact() {
             setMessage(inputValue);
         }
     };
+
+    const handleFormSubmit = (e) => {
+        // preventing the default behavior of the form submit (which is to refresh the page)
+        e.preventDefault();
+
+        // first we check to see if the email is not valid or if the userName is empty. If so, set an error message to be displayed on the page.
+        if (!validateEmail(email) || !userName) {
+            setErrorMessage("Email or Name is invalid");
+            // exit out of this code block if something is wrong so that the user can correct it
+            return;
+            // then check to see if the message is not valid. If so, set an error message regarding the message.
+        }
+
+        if (!setMessage(message)) {
+            setErrorMessage(`Message is required.`);
+            return;
+        }
+
+        // if everything goes according to plan, clear out the input after a successful submission.
+        setUserName("");
+        setMessage("");
+        setEmail("");
+    };
 }

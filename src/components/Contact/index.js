334cf0b5b469e524cb2788a52812a02137729754
savan <savannahwilliams243@gmail.com>
2024-02-24 11:25:21 -0800

    added contact html

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -48,4 +48,77 @@ function Contact() {
         setMessage("");
         setEmail("");
     };
+    return (
+        <section id="reach-out" className="contact">
+          <div className="flex-row">
+            <h2 className="section-title secondary-border">Reach Out</h2>
+          </div>
+    
+          <div className="contact-info">
+            <div>
+              <h3>Hello {userName}</h3>
+              <p>Want to get into contact?</p>
+              <address>
+                Alamo, CA <br />
+                P: <a href="tel:925.984.6707">925.984.6707</a>
+                <br />
+                E:{" "}
+                <a href="mailto://savannahwilliams243@gmail.com">
+                  savannahwilliams243@gmail.com
+                </a>
+              </address>
+              <p>
+                <strong>I'd love to hear your feedback!</strong>
+              </p>
+            </div>
+    
+            {/* contact form section  */}
+            <div className="contact-form">
+              <h3>Contact Me</h3>
+              <form className="form">
+                {/* Name */}
+                <label for="contact-name">Your Name</label>
+                <input
+                  value={userName}
+                  name="userName"
+                  onChange={handleInputChange}
+                  type="text"
+                  id="contact-name"
+                  placeholder="Your Name"
+                />
+    
+                {/* Email */}
+                <label for="contact-email">Your Email</label>
+                <input
+                  value={email}
+                  name="email"
+                  onChange={handleInputChange}
+                  type="email"
+                  id="contact-email"
+                  placeholder="Your Email"
+                />
+    
+                {/* Message */}
+                <label for="contact-message">Message</label>
+                <textarea
+                  value={message}
+                  name="message"
+                  onChange={handleInputChange}
+                  type="message"
+                  id="contact-message"
+                  placeholder="Your Message"
+                />
+                <button type="button" onClick={handleFormSubmit}>
+                  Submit
+                </button>
+              </form>
+            </div>
+            {errorMessage && (
+              <div>
+                <p className="error-text">{errorMessage}</p>
+              </div>
+            )}
+          </div>
+        </section>
+      );
 }
